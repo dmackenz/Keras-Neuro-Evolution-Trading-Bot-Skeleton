56105b6249c30c17e5fadec7ddcdc08417bb69a1
Kurtis Streutker <kurtisstreutker@gmail.com>
2018-05-12 19:35:29 -0400

    added some commenting

--- utils/Agent.py
+++ utils/Agent.py
@@ -16,10 +16,12 @@ class Agent(object):
         self.fitness = 0
         self.model = None
 
+        # object structs
         self.BUY = 1
         self.SELL = 2
         self.SLEEP = 3
 
+        # if mutating from an existing model
         if inherited_model:
             model_copy = clone_model(inherited_model)
             model_copy.set_weights(inherited_model.get_weights())
@@ -29,10 +31,9 @@ class Agent(object):
             self.model = self.population.model_builder()
 
     def batch_encode_prediction(self, predictions):
+        # converting output to trade signals
         encodeds = []
-
         for prediction in predictions:
-
             if np.argmax(prediction) == 0: encodeds.append(self.BUY)
             elif np.argmax(prediction) == 1: encodeds.append(self.SELL)
             else: encodeds.append(self.SLEEP)
@@ -40,28 +41,32 @@ class Agent(object):
         return encodeds
 
     def batch_act(self, inputs, prices):
-        inputs = np.array(inputs)
-        predictions = self.model.predict(inputs)
+        predictions = self.model.predict(np.array(inputs))
         encodeds = self.batch_encode_prediction(predictions)
 
+        # simulate trades based on trade signals
         for idx, encoded in enumerate(encodeds):
             if encoded == self.BUY:
                 self.wallet.buy(prices[idx])
             elif encoded == self.SELL:
                 self.wallet.sell(prices[idx])
 
+        # evaluate score
         self.score = self.wallet.get_swing_earnings(prices[-1])
 
     def save(self, filename):
         self.model.save_model(filename)
 
     def mutate(self):
+        # iterate through each layer of model
         for i in range(len(self.model.layers)):
             weights = self.model.layers[i].get_weights()
 
             # mutate weights of network
             for j in range(len(weights[0])):
                 for k in range(len(weights[0][j])):
+
+                    # randomly mutate based on mutation rate
                     if np.random.random() < self.population.mutation_rate:
                         weights[0][j][k] += np.random.normal(scale=self.population.mutation_scale) * 0.5
 
